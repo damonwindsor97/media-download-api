cd2768eec6d7752c9e6d2ff3a8e34ac9458561ec
Damon Windsor <damonwindsor@gmail.com>
2024-08-09 11:43:34 +1000

    Update youtube.js

--- api/youtube.js
+++ api/youtube.js
@@ -11,7 +11,7 @@ const ytdl = require('@distube/ytdl-core')
 
 const ffmpeg = require('ffmpeg-static');
 
-const agents = ytdl.createAgent ([
+const cookies = [
     {
         "domain": ".youtube.com",
         "expirationDate": 1756950891.189788,
@@ -264,7 +264,8 @@ const agents = ytdl.createAgent ([
         "value": "CgJBVRIEGgAgTA%3D%3D",
         "id": 18
     }
-    ])
+]
+const agent = ytdl.createAgent(cookies)
 
 // Route base/youtubeMp4
 router.route('/getTitle').post(async (req, res) => {
@@ -274,7 +275,7 @@ router.route('/getTitle').post(async (req, res) => {
         if(!ytdl.validateURL)
             return res.status(500).send("Not a valid link!")
 
-        const info = await ytdl.getInfo(videoUrl, { agents });
+        const info = await ytdl.getInfo(videoUrl, { agent });
         const title = info.videoDetails.title;
 
         res.status(200).send(title)
@@ -294,7 +295,7 @@ router.route('/downloadMp4').post(async (req, res) => {
         }
 
         // Get video info
-        const info = await ytdl.getInfo(videoUrl, { agents });
+        const info = await ytdl.getInfo(videoUrl, { agent });
         const title = info.videoDetails.title;
 
         console.log(`[MP4] Video successfully obtained: ${title}`);
@@ -319,23 +320,23 @@ router.route('/downloadMp4').post(async (req, res) => {
         });
 
         // Pipe video/audio streams into ffmpeg process
-        ytdl(videoUrl, { quality: 'highestaudio' }, { agents }).pipe(ffmpegProcess.stdio[4]);
-        ytdl(videoUrl, { quality: 'highestvideo' }, { agents }).pipe(ffmpegProcess.stdio[5]);
+        ytdl(videoUrl, { quality: 'highestaudio' }, { agent }).pipe(ffmpegProcess.stdio[4]);
+        ytdl(videoUrl, { quality: 'highestvideo' }, { agent }).pipe(ffmpegProcess.stdio[5]);
 
         // Listen for ffmpeg process close event
         ffmpegProcess.on('close', () => {
             console.log(`[MP4] Video successfully converted: ${title}`);
         
             // After ffmpegProcess is finished, send the file to the client
-            res.download(ffmpegPath, `${title}.mp4`, (err) => {
-                if (err) {
-                    console.error('[MP4] Download error:', err);
-                    res.status(500).send('Error downloading file');
+            res.download(ffmpegPath, `${title}.mp4`, (error) => {
+                if (error) {
+                    console.error('[MP4] Download error:', error);
+                    res.status(500).send('Error downloading file', error);
                 } else {
                     // Delete the temp video file after successful download
-                    fs.unlink(ffmpegPath, (unlinkErr) => {
-                        if (unlinkErr) {
-                            console.error('[MP4] Error deleting file:', unlinkErr);
+                    fs.unlink(ffmpegPath, (error) => {
+                        if (error) {
+                            console.error('[MP4] Error deleting file:', error);
                         } else {
                             console.log('[MP4] File deleted:', ffmpegPath);
                         }
@@ -374,7 +375,7 @@ router.route('/downloadMp3').post(async (req, res) => {
         if(!ytdl.validateURL(videoUrl))
             return res.status(500).send("Invalid YouTube URL")
     
-        const info = await ytdl.getInfo(videoUrl, { agents })
+        const info = await ytdl.getInfo(videoUrl, { agent })
         const title = info.videoDetails.title;
         console.log(`[MP3] Video successfully obtained: ${title}`)
 
@@ -393,7 +394,7 @@ router.route('/downloadMp3').post(async (req, res) => {
         const audioPath = path.join(process.cwd(), "temp", `${encodeURI(title)}.mp4`);
         const audioWriteStream = fs.createWriteStream(audioPath);
 
-        ytdl(videoUrl, { format: mp4Format }, { agents }).pipe(audioWriteStream);
+        ytdl(videoUrl, { format: mp4Format }, { agent }).pipe(audioWriteStream);
 
         res.set({
             'Content-Disposition': `attachment; filename="${title}.m4a"`, // Change filename extension to .mp3 | m4a so MacOS likes it
