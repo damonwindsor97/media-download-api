7dc628a15eeb881d86fce38444b42ca4faae45a9
Damon Windsor <damonwindsor@gmail.com>
2025-08-22 14:02:32 +1000

    Update serverController.js

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -11,6 +11,14 @@ const uid = function(){
     return `${new Date().getTime()}-${crypto.randomBytes(4).toString('hex')}`;
 };
 
+const getIp = (req) => {
+    return req.headers['x-forwarded-for']?.split(',')[0] || 
+           req.headers['x-real-ip'] || 
+           req.ip || 
+           req.connection?.remoteAddress || 
+           'unknown';
+};
+
 module.exports = {
 
     async getAnonToken(req, res) {
